28acbb9ca312aae60367a52e7efcccc328f72670
Trisha Sengupta <trishasengupta@Trishas-MacBook.local>
2018-03-06 21:05:29 -0800

    Analyze call
    Analyzes entire call instead of converting entire call to a string, analyzes each string separately

--- ems.py
+++ ems.py
@@ -1,14 +1,20 @@
 
+#imports csv module and accesses the file as hospital_summary
 import csv
 with open('911.csv', newline='') as hospital_summary:
     hospital_reader = csv.reader(hospital_summary)
+
+    #parses each row of data
     for row in hospital_reader:
 
+        #combines the list of strings in each row to one large string
         EMS = "EMS"
-        row_string = ' '.join(row)
 
-        if EMS in row_string:
-            print("EMS needed")
-        else:
-            print("EMS not needed")
+        for call in row:
+            if EMS in call:
+                print("EMS needed")
+            else:
+                print("EMS not needed")
+       
+
 
