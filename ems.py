1e44a9859c8b8cc7039e1cab6ea6ff220d5c528f
tsengupta715 <senguptatrisha15@gmail.com>
2018-03-06 19:41:29 -0800

    Add comments
    Added comments explaining what each part of the program does

--- ems.py
+++ ems.py
@@ -1,12 +1,16 @@
 
+#imports csv module and accesses the file as hospital_summary
 import csv
 with open('911.csv', newline='') as hospital_summary:
     hospital_reader = csv.reader(hospital_summary)
+    #parses each row of data 
     for row in hospital_reader:
 
+        #combines the list of strings in each row to one large string
         EMS = "EMS"
         row_string = ' '.join(row)
 
+        #detects whether or not EMS is in the string
         if EMS in row_string:
             print("EMS needed")
         else:
