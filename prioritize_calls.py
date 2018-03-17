2be23fb0ad1eca13e7bd9403e012fedbc7e7a345
Trisha Sengupta <trishasengupta@Trishas-MacBook.local>
2018-03-17 16:59:56 -0700

    Checks if call has symptoms
    Combines each call into one long string and compares the string against key and if it matches, returns the value

--- prioritize_calls.py
+++ prioritize_calls.py
@@ -1,14 +1,16 @@
 
-#inserts dictionary of priorities based upon MPDS
-import csv
 
+#inserts dictionary of priorities based upon MPDS
+
+import csv
 #opens key_words.csv as key_words
 with open('key_words.csv', 'rt') as key_words:
     #for each row, assigns symptoms as the first column and priority as the second column
     for row in key_words.readlines():
-        array = row.split(',')
-        symptoms = array[0]
-        priority = array[1].strip()
+        mpds_key = row.split(',')
+        symptoms = mpds_key[0]
+        priority = mpds_key[1].strip()
+
         #creates a dictionary as priority_dict with the key being the key words and the value is the priority
         priority_dict = {}
         priority_dict.update({symptoms:priority})
@@ -21,6 +23,7 @@ with open('911.csv', newline='') as summary_hospital:
     #parses each row of data and combines into one string
     for row in summary_reader:
         joined_call = ''.join(row)
+
         #checks if the string is in the symptoms and returns the priority if it is
         if symptoms.upper() in joined_call.upper():
             print(priority)
