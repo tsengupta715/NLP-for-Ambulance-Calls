e066c8636c264815a1d6df695a8ddba800616794
Trisha Sengupta <trishasengupta@Trishas-MacBook.local>
2018-03-17 15:09:26 -0700

    Creates dictionary
    creates dictionary using key words and mpds values

--- mpds_dictionary.py
+++ mpds_dictionary.py
@@ -3,9 +3,9 @@
 
 import csv
 
+
 #creates function which reads the csv module and creates a dictionary
 call_reader = csv.DictReader(open('mpds.csv','rt'))
 priority_list = []
 for call in call_reader:
     priority_list.append(call)
-    
