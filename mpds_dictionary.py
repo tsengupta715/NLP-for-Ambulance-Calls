9c64a92fe9bfecf063f4282996df27ac2f2d55d9
Trisha Sengupta <trishasengupta@Trishas-MacBook.local>
2018-03-17 16:44:22 -0700

    Creates Dictionary
    Creates dictionary with key words as the key and the value being the priority

--- mpds_dictionary.py
+++ mpds_dictionary.py
@@ -1,11 +1,14 @@
-
 #inserts dictionary of priorities based upon MPDS
-
 import csv
 
+#opens key_words.csv as key_words
+with open('key_words.csv', 'rt') as key_words:
+    #for each row, assigns symptoms as the first column and priority as the second column
+    for row in key_words.readlines():
+        array = row.split(',')
+        symptoms = array[0]
+        priority = array[1].strip()
+        #creates a dictionary as priority_dict with the key being the key words and the value is the priority
+        priority_dict = {}
+        priority_dict.update({symptoms:priority})
 
-#creates function which reads the csv module and creates a dictionary
-call_reader = csv.DictReader(open('mpds.csv','rt'))
-priority_list = []
-for call in call_reader:
-    priority_list.append(call)
